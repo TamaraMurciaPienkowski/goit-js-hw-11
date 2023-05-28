053911067fef2a52066d116783d6287a35d892f8
TamaraMurciaPienkowski <tamaramurciapienkowski@gmail.com>
2023-05-28 18:08:10 +0200

    finished task

--- src/index.js
+++ src/index.js
@@ -1,50 +1,138 @@
 import Notiflix from 'notiflix';
 import SimpleLightbox from 'simplelightbox';
+const loading = document.querySelector('.load');
+loading.classList.add('is-hidden');
 let lightbox;
+let page = 1;
+let q = '';
 const apiKey = '36752814-0630461e212967e8c9b2204d7';
 const apiStart = 'https://pixabay.com/api/';
 const searchForm = document.querySelector('.search-form');
-const bodyOdy = document.querySelector('body');
+const bodyOdy = document.querySelector('.gallery');
+
+// fetch gallery
+
 const getUrl = async (userRequest, page) => {
   const baseUrl = `${apiStart}?key=${apiKey}&q=${userRequest}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${page}`;
   const response = await fetch(baseUrl);
   const pictureArray = await response.json();
-  const picArray = pictureArray.hits;
-  return picArray;
+  // const picArray = pictureArray.hits;
+  return pictureArray;
 };
-function loadMorePics(page) {
-  pageNumber = page + 1;
-  getUrl(searchForm.searchQuery.value, pageNumber).then(picArray => {
-    const allPictures = picArray
+
+// load extra 40 pics
+
+function loadMorePics() {
+  page += 1;
+  lightbox.destroy();
+  q = searchForm.searchQuery.value;
+  console.log(page);
+  getUrl(q, page).then(pictureArray => {
+    const Pictures = pictureArray.hits
       .map(
-        galleryItem => `<li><a class="gallery__item" href="${galleryItem.largeImageURL}">
-    <img class = "gallery__image" src="${galleryItem.webformatURL}" alt="${galleryItem.tags}"/></a></li>`
+        gallery => `<a class= "gallery-link" href="${gallery.largeImageURL}"><div class="photo-card">
+        <img src="${gallery.webformatURL}" alt="${gallery.tags}" loading="lazy" />
+        <div class="info">
+        <p class="info-item">
+    <b>Likes</b> ${gallery.likes}
+  </p>
+  <p class="info-item">
+    <b>Views</b> ${gallery.views}
+  </p>
+  <p class="info-item">
+    <b>Comments</b> ${gallery.comments}
+  </p>
+  <p class="info-item">
+    <b>Downloads</b> ${gallery.downloads}
+  </p>
+        </div>
+        </div></a>`
       )
       .join('');
-    galleryBtn.insertAdjacentHTML('beforeend', allPictures);
+    bodyOdy.insertAdjacentHTML('beforeend', Pictures);
+    lightbox = new SimpleLightbox('a', {
+      captionsData: 'alt',
+      captionDelay: 250,
+    }).refresh();
+    const allPages = Math.ceil(pictureArray.totalHits / 40);
+    if (page >= allPages) {
+      loading.classList.add('is-hidden');
+      return Notiflix.Notify.failure(
+        "We're sorry, but you've reached the end of search results."
+      );
+    }
   });
 }
 
+// get first 40 pics
+
 function galeryCreator(e) {
   e.preventDefault();
-  getUrl(searchForm.searchQuery.value, 1).then(picArray => {
-    const allPictures = picArray
-      .map(
-        galleryItem => `<li><a class="gallery__item" href="${galleryItem.largeImageURL}">
-    <img class = "gallery__image" src="${galleryItem.webformatURL}" alt="${galleryItem.tags}"/></a></li>`
-      )
-      .join('');
-    lightbox = new SimpleLightbox('.gallery a', {
-      captionsData: 'alt',
-      captionDelay: 250,
-    });
-    bodyOdy.insertAdjacentHTML('beforeend', allPictures);
-    bodyOdy.insertAdjacentHTML(
-      'beforeend',
-      '<button class="gallery__button">Load more</button>'
-    );
+  bodyOdy.innerHTML = '';
+  loading.classList.add('is-hidden');
+  page = 1;
+  q = searchForm.searchQuery.value;
+
+  getUrl(q, page).then(pictureArray => {
+    if (pictureArray.totalHits === 0) {
+      Notiflix.Notify.failure(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+    } else {
+      const allPictures = pictureArray.hits
+        .map(
+          galleryItem => `<a class= "gallery-link" href="${galleryItem.largeImageURL}"><div class="photo-card">
+          <img src="${galleryItem.webformatURL}" alt="${galleryItem.tags}" loading="lazy" />
+          <div class="info">
+          <p class="info-item">
+      <b>Likes</b> ${galleryItem.likes}
+    </p>
+    <p class="info-item">
+      <b>Views</b> ${galleryItem.views}
+    </p>
+    <p class="info-item">
+      <b>Comments</b> ${galleryItem.comments}
+    </p>
+    <p class="info-item">
+      <b>Downloads</b> ${galleryItem.downloads}
+    </p>
+          </div>
+          </div></a>`
+        )
+        .join('');
+      console.log(pictureArray);
+      bodyOdy.insertAdjacentHTML('beforeend', allPictures);
+      lightbox = new SimpleLightbox('a', {
+        captionsData: 'alt',
+        captionDelay: 250,
+      }).refresh();
+      if (pictureArray.totalHits > 40) {
+        loading.classList.remove('is-hidden');
+      }
+    }
   });
 }
+
+{
+  /* <div class="photo-card">
+  <img src="" alt="" loading="lazy" />
+  <div class="info">
+    <p class="info-item">
+      <b>Likes</b>
+    </p>
+    <p class="info-item">
+      <b>Views</b>
+    </p>
+    <p class="info-item">
+      <b>Comments</b>
+    </p>
+    <p class="info-item">
+      <b>Downloads</b>
+    </p>
+  </div>
+</div> */
+}
+// event listener
+
 searchForm.addEventListener('submit', galeryCreator);
-const galleryBtn = document.querySelector('.gallery__button');
-galleryBtn.addEventListener('click', loadMorePics(page));
+loading.addEventListener('click', loadMorePics);
