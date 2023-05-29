a3b581e07183df16c52ac528488a966f88a57065
TamaraMurciaPienkowski <tamaramurciapienkowski@gmail.com>
2023-05-29 15:44:51 +0200

    added gallery styles

--- src/index.js
+++ src/index.js
@@ -114,26 +114,5 @@ function galeryCreator(e) {
   });
 }
 
-{
-  /* <div class="photo-card">
-  <img src="" alt="" loading="lazy" />
-  <div class="info">
-    <p class="info-item">
-      <b>Likes</b>
-    </p>
-    <p class="info-item">
-      <b>Views</b>
-    </p>
-    <p class="info-item">
-      <b>Comments</b>
-    </p>
-    <p class="info-item">
-      <b>Downloads</b>
-    </p>
-  </div>
-</div> */
-}
-// event listener
-
 searchForm.addEventListener('submit', galeryCreator);
 loading.addEventListener('click', loadMorePics);
